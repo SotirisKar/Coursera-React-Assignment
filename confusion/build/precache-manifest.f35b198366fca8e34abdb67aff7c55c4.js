d9ec2d6d240d48c524823240ab4fd6b64bb25823
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2020-09-28 23:35:09 +0300

    Add files via upload

---
+++ confusion/build/precache-manifest.f35b198366fca8e34abdb67aff7c55c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d9d3864c097b05eb03d5c7f302f20e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a669b379cbdfd6f9f70b",
+    "url": "/static/css/2.b0e1eb24.chunk.css"
+  },
+  {
+    "revision": "d534285a698056607cc7",
+    "url": "/static/css/main.dce9b359.chunk.css"
+  },
+  {
+    "revision": "a669b379cbdfd6f9f70b",
+    "url": "/static/js/2.59fed99b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.59fed99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d534285a698056607cc7",
+    "url": "/static/js/main.900db9d1.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ confusion/src/shared/baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = 'httpL//localhost:3001/';
